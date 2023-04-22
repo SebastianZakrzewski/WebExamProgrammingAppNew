01eef2f4dc808caafc6c511a6de5229c17bcd899
sebastian <sebastianzakrzewski1995@gmai.com>
2023-04-22 19:52:59 +0200

    Add Back-End server

---
+++ Back-End/src/main/java/sebastianzakrzewski/examprogramming/ExamProgrammingApplication.java
@@ -0,0 +1,13 @@
+package sebastianzakrzewski.examprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamProgrammingApplication.class, args);
+    }
+
+}
---
+++ Back-End/src/test/java/sebastianzakrzewski/examprogramming/ExamProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package sebastianzakrzewski.examprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
